2247a5608b44665a58e63b543d82a1e22ad65cbd
Matthias Mayer <matthias.mayer@tum.de>
2023-02-13 20:29:50 +0100

    FIX Add timeout to schema + robot fetch from cobra website

--- src/timor/utilities/download_data.py
+++ src/timor/utilities/download_data.py
@@ -12,7 +12,7 @@ from timor.utilities import logging
 def download_schemata(schema_dir: Path) -> Dict[str, Path]:
     """Download schemata from CoBRA; return dict from schema short name to paths where stored."""
     schemata = {}
-    wanted_schemas = ('PoseSchema', 'TaskSchema', 'ModuleSchema', 'SolutionSchema')
+    wanted_schemas = {'PoseSchema', 'TaskSchema', 'ModuleSchema', 'SolutionSchema'}
     for s in schema_dir.iterdir():
         if s.suffix != ".json":
             continue
@@ -21,15 +21,17 @@ def download_schemata(schema_dir: Path) -> Dict[str, Path]:
 
     # --- Begin download schemata ---
     tld = "https://cobra.cps.cit.tum.de/api/schemas/"
-    for schema in ('PoseSchema', 'TaskSchema', 'ModuleSchema', 'SolutionSchema') - schemata.keys():
+    for schema in wanted_schemas - schemata.keys():
         url = tld + schema + '.json'
         logging.info(f"Downloading schema from {url}")
         try:
-            r = requests.get(url)
+            r = requests.get(url, timeout=2.)
             schema_file_name = schema_dir.joinpath(schema + '.json')
             with open(schema_file_name, 'wb') as f:
                 f.write(r.content)
             schemata[schema] = schema_file_name
+        except TimeoutError:
+            logging.warning("Could not get updated schemata from CoBRA website.")
         except Exception as e:
             logging.warning(f"Could not download from {url}: {e}")
     # --- End download schemata ---
@@ -41,7 +43,10 @@ def download_additional_robots(default_robot_dir: Path, robots: Dict[str, Path])
     robot_tld = "https://cobra.cps.cit.tum.de/api/robots"
     additional_robots = set()
     try:
-        additional_robots = set(json.loads(requests.get(robot_tld).content))
+        additional_robots = set(json.loads(requests.get(robot_tld, timeout=2.).content))
+    except TimeoutError:
+        logging.warning("Could not fetch additional robots from CoBRA website.")
+        return
     except Exception as e:
         logging.warning(f"Could not fetch additional robots due to {e}.")
 
@@ -57,10 +62,13 @@ def download_additional_robots(default_robot_dir: Path, robots: Dict[str, Path])
             continue
         logging.info(f"Getting robot {additional_robot} from {robot_tld}.")
         try:
-            r = requests.get(f"{robot_tld}/{additional_robot}?zip=True")
+            r = requests.get(f"{robot_tld}/{additional_robot}?zip=True", timeout=10.)  # Big files - more time
             zip_file = zipfile.ZipFile(io.BytesIO(r.content))
             zip_file.extractall(output_dir)
             robots[additional_robot] = output_dir
             logging.info(f"Stored as {additional_robot} in {output_dir}.")
+        except TimeoutError:
+            logging.warning(f"Could not fetch {additional_robot} from CoBRA website.")
+            continue
         except Exception as e:
             logging.warning(f"Could not fetch robot {additional_robot} due to {e}.")
